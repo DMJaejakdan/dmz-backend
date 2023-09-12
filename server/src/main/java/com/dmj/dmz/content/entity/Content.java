2fb961c2cbe9a6f634b9e5a1c3840713b2846c2b
klkim1913 <krkim1913@gmail.com>
2023-09-12 16:08:19 +0900

    fix: 오류 수정

--- server/src/main/java/com/dmj/dmz/content/entity/Content.java
+++ server/src/main/java/com/dmj/dmz/content/entity/Content.java
@@ -68,7 +68,7 @@ public class Content {
                 }
             }
             String s = dramaResultResponse.getFirstAirDate();
-            if (!s.isEmpty())
+            if (s != null && !s.isEmpty())
                 releasedDate = LocalDate.parse(s);
             nameKR = dramaResultResponse.getOriginalName();
             nameEn = dramaResultResponse.getName();
