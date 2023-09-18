df817bbd0889c3583008d906efe1028cdfa41e38
serenecarp <spmjoon516@gmail.com>
2023-09-18 11:23:18 +0900

    feat: Txt 컴포넌트 제작

---
+++ client/_lib/components/txt/Txt.css.ts
@@ -0,0 +1,47 @@
+import { theme } from '#/design/theme.css';
+import { style, styleVariants } from '@vanilla-extract/css';
+
+const base = style({
+  color: theme.txt.white,
+});
+
+const fontSizeVar = {
+  8: '0.5rem',
+  10: '0.625rem',
+  12: '0.75rem',
+  13: '0.8125rem',
+  14: '0.875rem',
+  16: '1rem',
+  18: '1.125rem',
+  20: '1.25rem',
+  24: '1.5rem',
+  28: '1.75rem',
+  32: '2rem',
+};
+
+export const SIZE_VARIANT = styleVariants(fontSizeVar, fontSize => [
+  base,
+  { fontSize: fontSize, marginBottom: fontSize },
+]);
+
+const fontWeightsVar = {
+  regular: '400',
+  bold: '700',
+};
+
+export const WEIGHT_VARIANT = styleVariants(fontWeightsVar, fontWeights => [
+  { fontWeight: fontWeights },
+]);
+
+export const ALIGN_VARIANT = styleVariants({
+  start: { textAlign: 'start' },
+  center: { textAlign: 'center' },
+  end: { textAlign: 'end' },
+  left: { textAlign: 'start' },
+  right: { textAlign: 'end' },
+});
+
+export const COLOR_VARIANT = styleVariants({
+  black: { color: theme.txt.black },
+  white: { color: theme.txt.white },
+});
