84aa864cd56fa4e35a40738d11782ea3c258a330
brewcoldblue <seung.yoon.yu@gmail.com>
2023-09-19 15:29:57 +0900

    feat: 서버 타임존 설정

--- client/next.config.js
+++ client/next.config.js
@@ -5,3 +5,20 @@ const withVanillaExtract = createVanillaExtractPlugin();
 const nextConfig = {};
 
 module.exports = withVanillaExtract(nextConfig);
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+// 서버 타임존을 설정합니다. 예: 서울 시간대
+process.env.TZ = 'Asia/Seoul';
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3000, err => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
