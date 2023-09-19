6687147464312aca5ac9b2cd9232d3283813bee4
brewcoldblue <seung.yoon.yu@gmail.com>
2023-09-19 16:35:05 +0900

    fix: 빌드 오류 해결

--- client/next.config.js
+++ client/next.config.js
@@ -1,23 +1,8 @@
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
+process.env.TZ = 'Asia/Seoul';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 module.exports = withVanillaExtract(nextConfig);
-
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-process.env.TZ = 'Asia/Seoul';
-
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(3000, err => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
-});
