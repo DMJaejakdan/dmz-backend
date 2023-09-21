69567645c05075415590b23f1bf3ed46a70640ff
bmincof <bminlvcoffee@gmail.com>
2023-09-21 09:55:55 +0900

    feat: 양방향 간선 생성 기능 추가

--- data-processing/src/main/java/com/dmj/dmz/Main.java
+++ data-processing/src/main/java/com/dmj/dmz/Main.java
@@ -6,7 +6,6 @@ import com.dmj.dmz.session.SparkMongoSession;
 import com.dmj.dmz.session.SparkMysqlSession;
 import org.apache.spark.api.java.JavaRDD;
 
-// TODO: aws 서버의 데이터베이스로 추가하기
 public class Main {
     private static final SparkMysqlSession mysqlSession = SparkMysqlSession.getInstance();
     private static final SparkMongoSession mongoSession = SparkMongoSession.getInstance();
@@ -16,51 +15,53 @@ public class Main {
         makeVerticesAndSave();
 
         // 간선 collection 만들기
+        // 단방향
         // 작품-인물 간선
         makeContentPersonRelationsAndSave();
-        makeContentContentRelationsAndSave();
 
         // 작품-작품 간선
         makeContentContentRelationsAndSave();
 
-        // 인물-인물 간선
-
+        // 양방향
+        makeUndirectedContentContentRelationsAndSave();
     }
 
     // Mysql의 content, person 테이블을 Mongodb의 vertex document 형식으로 변환시켜 vertex collection에 저장
     private static void makeVerticesAndSave() {
         JavaRddTransformer transformer = new JavaRddTransformer();
 
-        JavaRDD<Vertex> contentDataset =
-                transformer.transformContentToJavaRDD(mysqlSession.readTable("content"));
-        mongoSession.appendVerticesToMongoCollection(contentDataset);
+        JavaRDD<Vertex> contentDataset = transformer.transformContentToJavaRDD(mysqlSession.readTable("content"));
+        mongoSession.overwriteToMongoCollection(contentDataset, Vertex.class, "vertex");
 
-        JavaRDD<Vertex> personDataset =
-                transformer.transformPersonToJavaRDD(mysqlSession.readTable("person"));
-        mongoSession.appendVerticesToMongoCollection(personDataset);
+        JavaRDD<Vertex> personDataset = transformer.transformPersonToJavaRDD(mysqlSession.readTable("person"));
+        mongoSession.appendToMongoCollection(personDataset, Vertex.class, "vertex");
     }
 
     // Mysql의 content_actor, content_crew 테이블을 Mongodb의 edge 형식으로 변환시켜 edge collection에 저장
     private static void makeContentPersonRelationsAndSave() {
         JavaRddTransformer transformer = new JavaRddTransformer();
 
-        JavaRDD<Edge> contentActorDataset =
-                transformer.transformContentActorToJavaRDD(mysqlSession.readTable("content_actor"));
-        mongoSession.appendEdgesToMongoCollection(contentActorDataset);
+        JavaRDD<Edge> contentActorDataset = transformer.transformContentActorToJavaRDD(mysqlSession.readTable("content_actor"));
+        mongoSession.overwriteToMongoCollection(contentActorDataset, Edge.class, "edge");
 
-        JavaRDD<Edge> contentCrewDataset =
-                transformer.transformContentCrewToJavaRDD(mysqlSession.readTable("content_crew"));
-        mongoSession.appendEdgesToMongoCollection(contentCrewDataset);
+        JavaRDD<Edge> contentCrewDataset = transformer.transformContentCrewToJavaRDD(mysqlSession.readTable("content_crew"));
+        mongoSession.appendToMongoCollection(contentCrewDataset, Edge.class, "edge");
     }
 
     // Mysql의 content 테이블을 Mongodb의 edge 형식으로 변환시켜 edge collection에 저장
     private static void makeContentContentRelationsAndSave() {
         JavaRddTransformer transformer = new JavaRddTransformer();
 
-        JavaRDD<Edge> contentContentDataset =
-                transformer.transformContentContentToJavaRDD(mysqlSession.readTable("content_genre"),
-                        mysqlSession.readTable("content_keyword"));
+        JavaRDD<Edge> contentContentDataset = transformer.transformContentContentToJavaRDD(mysqlSession.readTable("content_genre"), mysqlSession.readTable("content_keyword"));
 
-        mongoSession.appendEdgesToMongoCollection(contentContentDataset);
+        mongoSession.appendToMongoCollection(contentContentDataset, Edge.class, "edge");
+    }
+
+    private static void makeUndirectedContentContentRelationsAndSave() {
+        JavaRddTransformer transformer = new JavaRddTransformer();
+
+        JavaRDD<Edge> contentContentDataset = transformer.transformUndirectedContentContentToJavaRDD(mysqlSession.readTable("content_genre"), mysqlSession.readTable("content_keyword"));
+
+        mongoSession.overwriteToMongoCollection(contentContentDataset, Edge.class, "undirected_edge");
     }
 }
