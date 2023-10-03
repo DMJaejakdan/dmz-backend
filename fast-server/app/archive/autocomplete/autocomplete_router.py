6fc37281b8149bb23842f8ace72a56ddab4bbe04
Yejining <kimyejin.kr@gmail.com>
2023-10-03 21:25:14 +0900

    S09P22A602-206/feat: add autocomplate package
    Former-commit-id: d1e374915821556ac92dfa75a178af9f17751fe1

---
+++ fast-server/app/archive/autocomplete/autocomplete_router.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends, HTTPException
+from elasticsearch import AsyncElasticsearch
+
+from app.dependency import get_client
+
+
+router = APIRouter(
+    prefix='/fapi/auto'
+)
+
+
+@router.get("/search")
+async def search(query: str, client: AsyncElasticsearch = Depends(get_client)):
+    try:
+        result = client.search(index='your_index', q=query)
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- fast-server/app/main.py
+++ fast-server/app/main.py
@@ -9,6 +9,7 @@ from app.dependency import close_client
 from app.archive.movie import movie_router
 from app.archive.drama import drama_router
 from app.archive.people import people_router
+from app.archive.autocomplete import autocomplete_router
 
 app = FastAPI()
 
@@ -17,6 +18,7 @@ server_timezone = pytz.timezone('Asia/Seoul')
 app.include_router(movie_router.router)
 app.include_router(drama_router.router)
 app.include_router(people_router.router)
+app.include_router(autocomplete_router.router)
 
 query_builder = ElasticsearchAPIQueryBuilder()
 
