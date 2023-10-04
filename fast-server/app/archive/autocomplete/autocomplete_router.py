ac6a98fd01ebe36eefeedf81ca65b252d4bef294
Yejining <kimyejin.kr@gmail.com>
2023-10-04 11:18:43 +0900

    S09P22A602-210/refactor: update autocomplete API for genres
    Former-commit-id: 92b869a86c5363309ebf69ad4167d59101a8cb9e

--- fast-server/app/archive/autocomplete/autocomplete_router.py
+++ fast-server/app/archive/autocomplete/autocomplete_router.py
@@ -21,24 +21,12 @@ async def fields(client: AsyncElasticsearch = Depends(get_client),
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get('/movie/genres/{genre}')
-async def movie_genres(genre: str,
-                       client: AsyncElasticsearch = Depends(get_client()),
-                       index: str = Depends(get_genre_index)):
+@router.get('/{video_type}/genres/{genre}')
+async def genres(video_type: VideoType, genre: str,
+                 client: AsyncElasticsearch = Depends(get_client()),
+                 index: str = Depends(get_genre_index)):
     try:
-        query = get_genre_query(genre, VideoType.MOVIE)
-        result = await client.search(index=index, query=query)
-        return result
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@router.get('/drama/genres/{genre}')
-async def movie_genres(genre: str,
-                       client: AsyncElasticsearch = Depends(get_client()),
-                       index: str = Depends(get_genre_index)):
-    try:
-        query = get_genre_query(genre, VideoType.DRAMA)
+        query = get_genre_query(genre, video_type)
         result = await client.search(index=index, query=query)
         return result
     except Exception as e:
