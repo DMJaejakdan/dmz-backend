028c89b50815bb6c688ec253227d821e0069e1ec
brewcoldblue <seung.yoon.yu@gmail.com>
2023-10-04 09:22:56 +0900

    fix: storybook 설정 및 다른 잘못된 설정 수정
    Former-commit-id: 4ed5fa3974da7dd5be86a3b9c5d0e9731e5e83b6

--- client/frame/.pnp.cjs
+++ client/frame/.pnp.cjs
@@ -48,7 +48,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["prettier", "npm:3.0.3"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:0d5adf23af2c07be1ac12dfd8479419c25c352e900570741b78d4db2f94f332d98ea7c591d77900527df6f90283421c8ffb1643d864630727c7fd10476ab4daf#npm:18.2.0"],\
-            ["router", "file:../router#../router::hash=d15b99&locator=frame%40workspace%3A."],\
+            ["router", "file:../router#../router::hash=dc0060&locator=frame%40workspace%3A."],\
             ["typescript", "patch:typescript@npm%3A5.2.2#~builtin<compat/typescript>::version=5.2.2&hash=f3b441"],\
             ["webpack", "virtual:0d5adf23af2c07be1ac12dfd8479419c25c352e900570741b78d4db2f94f332d98ea7c591d77900527df6f90283421c8ffb1643d864630727c7fd10476ab4daf#npm:5.80.0"]\
           ],\
@@ -3858,7 +3858,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["prettier", "npm:3.0.3"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:0d5adf23af2c07be1ac12dfd8479419c25c352e900570741b78d4db2f94f332d98ea7c591d77900527df6f90283421c8ffb1643d864630727c7fd10476ab4daf#npm:18.2.0"],\
-            ["router", "file:../router#../router::hash=d15b99&locator=frame%40workspace%3A."],\
+            ["router", "file:../router#../router::hash=dc0060&locator=frame%40workspace%3A."],\
             ["typescript", "patch:typescript@npm%3A5.2.2#~builtin<compat/typescript>::version=5.2.2&hash=f3b441"],\
             ["webpack", "virtual:0d5adf23af2c07be1ac12dfd8479419c25c352e900570741b78d4db2f94f332d98ea7c591d77900527df6f90283421c8ffb1643d864630727c7fd10476ab4daf#npm:5.80.0"]\
           ],\
@@ -6202,10 +6202,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["router", [\
-        ["file:../router#../router::hash=d15b99&locator=frame%40workspace%3A.", {\
-          "packageLocation": "./.yarn/cache/router-file-aa0a730f36-8906471026.zip/node_modules/router/",\
+        ["file:../router#../router::hash=dc0060&locator=frame%40workspace%3A.", {\
+          "packageLocation": "./.yarn/cache/router-file-34e4d4984c-222a60ccdc.zip/node_modules/router/",\
           "packageDependencies": [\
-            ["router", "file:../router#../router::hash=d15b99&locator=frame%40workspace%3A."],\
+            ["router", "file:../router#../router::hash=dc0060&locator=frame%40workspace%3A."],\
             ["feather-route-matcher", "npm:4.0.0"]\
           ],\
           "linkType": "HARD"\
