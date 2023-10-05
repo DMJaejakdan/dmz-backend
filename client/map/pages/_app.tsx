b4e9f15a0b0bbcdd2227b3cb56729d759fd5b460
brewcoldblue <seung.yoon.yu@gmail.com>
2023-10-05 23:14:22 +0900

    feat: 아카이브로 갈 수 있는 버튼 추가
    Former-commit-id: 6c29a228fc7251ecc47234b239375b44f454b591

--- client/map/pages/_app.tsx
+++ client/map/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { AppProps } from 'next/app';
+
+const Nav = dynamic(() => import('dmzlib/Nav'), { ssr: false });
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Link href={process.env.NEXT_PUBLIC_ROOT as string}>
+        <Nav text="드라마영화아카이브로 가기" />
+      </Link>
       <Component {...pageProps} />
     </>
   );
 }
 
 export default MyApp;
+import Link from 'next/link';
+import dynamic from 'next/dynamic';
