12068d25084f834b22fad71d068bb89d68fa4787
jeong-yeji <jeongyeji.mail@gmail.com>
2023-10-05 17:27:16 +0900

    chore: es log 저장
    Former-commit-id: ed70a3ba57c4c749f1dd943638aedaf6604f9722

--- elk/es_index.sh
+++ elk/es_index.sh
@@ -14,17 +14,17 @@ mkdir ./logs/index
 
 curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/genre" -H "Content-Type: application/json" -d @$INDEX_PATH/genre.json -o ./logs/index/genre.log
 echo ""
-curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/keyword" -H "Content-Type: application/json" -d @$INDEX_PATH/keyword.json ./logs/index/keyword.log
+curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/keyword" -H "Content-Type: application/json" -d @$INDEX_PATH/keyword.json -o ./logs/index/keyword.log
 echo ""
-curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/company" -H "Content-Type: application/json" -d @$INDEX_PATH/company.json ./logs/index/company.log
+curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/company" -H "Content-Type: application/json" -d @$INDEX_PATH/company.json -o ./logs/index/company.log
 echo ""
-curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/channel" -H "Content-Type: application/json" -d @$INDEX_PATH/channel.json ./logs/index/channel.log
+curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/channel" -H "Content-Type: application/json" -d @$INDEX_PATH/channel.json -o ./logs/index/channel.log
 echo ""
-curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/field" -H "Content-Type: application/json" -d @$INDEX_PATH/field.json ./logs/index/field.log
+curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/field" -H "Content-Type: application/json" -d @$INDEX_PATH/field.json -o ./logs/index/field.log
 echo ""
-curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/movie" -H "Content-Type: application/json" -d @$INDEX_PATH/movie.json ./logs/index/movie.log
+curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/movie" -H "Content-Type: application/json" -d @$INDEX_PATH/movie.json -o ./logs/index/movie.log
 echo ""
-curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/drama" -H "Content-Type: application/json" -d @$INDEX_PATH/drama.json ./logs/index/drama.log
+curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/drama" -H "Content-Type: application/json" -d @$INDEX_PATH/drama.json -o ./logs/index/drama.log
 echo ""
-curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/people" -H "Content-Type: application/json" -d @$INDEX_PATH/people.json ./logs/index/people.log
+curl -X PUT -u elastic:$ELK_PASSWORD "http://localhost:3200/people" -H "Content-Type: application/json" -d @$INDEX_PATH/people.json -o ./logs/index/people.log
 echo ""
