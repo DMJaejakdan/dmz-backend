91d69914c6adaab1d6b4af652f2e5664bdf4031b
serenecarp <spmjoon516@gmail.com>
2023-10-06 02:57:21 +0900

    feat: 페이지에 들어갈 상수 설정
    Former-commit-id: bf5c53afc6062077c069d7e026c053ac996e4522

---
+++ client/archive/constants/detail.ts
@@ -0,0 +1,30 @@
+export default {
+  drama: {
+    release: '방영일',
+    startDate: '첫 방송',
+    endDate: '마지막 방송',
+
+    genre: '장르',
+    keyword: '키워드',
+    makers: '제작진',
+    actors: '출연진',
+
+    rating: '평가',
+    companies: '제작사',
+    channel: '채널',
+  },
+  movie: {
+    release: '개봉일',
+    genre: '장르',
+    keyword: '키워드',
+    makers: '제작진',
+    actors: '출연진',
+
+    rating: '평가',
+    companies: '제작사',
+  },
+  person: {
+    birth: '출생',
+    filmography: '필모그래피',
+  },
+};
