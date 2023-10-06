ef24358d678594cff577f4e1a51cd75b0a651437
brewcoldblue <seung.yoon.yu@gmail.com>
2023-10-06 12:45:58 +0900

    fix: 그래프 viewbox 삭제 및 api 라우트 변경
    Former-commit-id: d078a16463b71563b6b64403f1b904803df00f34

--- client/map/pages/Segment.tsx
+++ client/map/pages/Segment.tsx
@@ -17,48 +17,8 @@ function Graph({ vertices, edges }: { vertices: any; edges: any }) {
 }
 export async function getStaticProps() {
   const { vertices, edges } = await fetch(
-    `${process.env.NEXT_PUBLIC_ROOT2}/dmzmap/api/getInitialData`
+    'https://j9a602a.p.ssafy.io/dmzmap/api/getInitialData'
   ).then(response => response.json());
   return { props: { vertices, edges }, revalidate: 300 };
 }
 export default Segment;
-
-// export async function getStaticPaths() {
-//   const client = await MongoClient.connect(
-//     'mongodb://root:ssafy.j9a602.dmz@j9a602.p.ssafy.io:27017'
-//   );
-//   const db = client.db('DMZ');
-//   const edgeCollection = db.collection('edge');
-
-//   // 전체 간선 수를 가져옵니다.
-//   const totalEdges = await edgeCollection.countDocuments({
-//     type: 'content-content',
-//   });
-
-//   // 페이지 수를 계산합니다. 여기서는 간선 200개마다 페이지를 분할합니다.
-//   const pages = Math.ceil(JSON.parse(JSON.stringify(totalEdges)) / 1000);
-
-//   const paths = Array.from({ length: pages }).map((_, i) => ({
-//     params: { segment: `${i * 1000}-${(i + 1) * 1000 - 1}` },
-//   }));
-
-//   client.close();
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
-
-// export async function getStaticProps({ params }: any) {
-//   const [start, end] = params.segment.split('-').map(Number);
-//   console.log('데이터 가져옴...');
-//   const skip = start;
-//   const limit = end - start + 1;
-//   const { vertices, edges } = await getGraphData(limit, skip);
-
-//   return {
-//     props: { vertices, edges },
-//     revalidate: 86400,
-//   };
-// }
